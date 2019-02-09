a339d79055e95d34984f306c0fbf17767a2c4036
Nic Pham <nicholascatpham@gmail.com>
2019-02-09 10:57:13 -0800

    fixed up cargo constants

---
+++ src/main/java/frc/team3256/robot/constants/CargoConstants.java
@@ -0,0 +1,22 @@
+package frc.team3256.robot.constants;
+
+public class CargoConstants {
+    public static final int kIntake = 0;
+    public static final int kPivot = 0;
+
+    public static final double kPivotP = 0;
+    public static final double kPivotI = 0;
+    public static final double kPivotD = 0;
+    public static final double kPivotF = 0;
+    public static final double kPivotIz = 0;
+
+    public static final double kPositionInside = 0;
+    public static final double kPositionTransfer = 0;
+    public static final double kPositionClearance = 0;
+    public static final double kPositionFloor = 0;
+
+    public static final double kIntakeSpeed = 0;
+    public static final double kPivotSpeed = 0;
+    public static final double kPivotMaxOutput = 1;
+    public static final double kPivotMinOutput = -1;
+}
--- src/main/java/frc/team3256/robot/subsystems/CargoIntake.java
+++ src/main/java/frc/team3256/robot/subsystems/CargoIntake.java
@@ -2,34 +2,29 @@ package frc.team3256.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.*;
-import edu.wpi.first.wpilibj.Spark;
-import frc.team3256.robot.operations.Constants;
 import frc.team3256.warriorlib.hardware.SparkMAXUtil;
 import frc.team3256.warriorlib.hardware.TalonSRXUtil;
 import frc.team3256.warriorlib.subsystem.SubsystemBase;
 
+import static frc.team3256.robot.constants.CargoConstants.*;
+
 public class CargoIntake extends SubsystemBase {
 
 	private static CargoIntake instance;
-	private WPI_TalonSRX cargoIntake, cargoScoreLeft, cargoScoreRight;
+	private WPI_TalonSRX cargoIntake;
 	private CANSparkMax cargoPivot;
 	private CANPIDController cargoPID;
 	private CANEncoder cargoEncoder;
 
 	private CargoIntake() {
-		cargoIntake = TalonSRXUtil.generateGenericTalon(Constants.kCargoIntakePort);
-		cargoScoreLeft = TalonSRXUtil.generateGenericTalon(Constants.kCargoScoreLeftPort);
-		cargoScoreRight = TalonSRXUtil.generateGenericTalon(Constants.kCargoScoreRightPort);
-		cargoPivot = SparkMAXUtil.generateGenericSparkMAX(Constants.kCargoPivotPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+		cargoIntake = TalonSRXUtil.generateGenericTalon(kIntake);
+		cargoPivot = SparkMAXUtil.generateGenericSparkMAX(kPivot, CANSparkMaxLowLevel.MotorType.kBrushless);
 		SparkMAXUtil.setBrakeMode(cargoPivot);
 		cargoPID = cargoPivot.getPIDController();
 		cargoEncoder = cargoPivot.getEncoder();
 
-		SparkMAXUtil.setPIDGains(cargoPID, Constants.kCargoPivotUpSlot, Constants.kCargoPivotUpP, Constants.kCargoPivotUpI, Constants.kCargoPivotUpD, Constants.kCargoPivotUpF, Constants.kCargoPivotUpIz);
-
-		SparkMAXUtil.setPIDGains(cargoPID, Constants.kCargoPivotDownSlot, Constants.kCargoPivotDownP, Constants.kCargoPivotDownI, Constants.kCargoPivotDownD, Constants.kCargoPivotDownF, Constants.kCargoPivotDownIz);
-
-		cargoPID.setOutputRange(Constants.kCargoPivotMinOutput, Constants.kCargoPivotMaxOutput);
+		SparkMAXUtil.setPIDGains(cargoPID, 0, kPivotP, kPivotI, kPivotD, kPivotF, kPivotIz);
+		cargoPID.setOutputRange(kPivotMinOutput, kPivotMaxOutput);
 	}
 
 	public static CargoIntake getInstance() {
@@ -39,13 +34,6 @@ public class CargoIntake extends SubsystemBase {
 	@Override
 	public void update(double timestamp) {
 		System.out.println("Cargo Pivot Pos: " + getPosition());
-		if (getPosition() > Constants.kCargoPivotClearancePreset) {
-			cargoPivot.set(0);
-			setPosition(Constants.kCargoPivotClearancePreset);
-		} else if (getPosition() < Constants.kCargoPivotClearancePreset) {
-			cargoPivot.set(0);
-			setPosition(Constants.kCargoPivotClearancePreset);
-		}
 	}
 
 	public void setPositionFoldIn() {
@@ -68,11 +56,6 @@ public class CargoIntake extends SubsystemBase {
 		cargoIntake.set(power);
 	}
 
-	public void setScorePower(double power) {
-		cargoScoreLeft.set(power);
-		cargoScoreRight.set(power);
-	}
-
 	public void setPivotPower(double power) {
 		cargoPivot.set(power);
 	}
@@ -92,7 +75,7 @@ public class CargoIntake extends SubsystemBase {
 
 	@Override
 	public void zeroSensors() {
-
+		cargoEncoder.setPosition(0);
 	}
 
 	@Override
