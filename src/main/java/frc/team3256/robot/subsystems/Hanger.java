33fcb931b064eed80ce1a155c7b7cd50d4a532c6
albertzhao629 <albertzhao629@gmail.com>
2019-03-19 16:30:22 -0700

    started hanger state system

--- src/main/java/frc/team3256/robot/subsystems/Hanger.java
+++ src/main/java/frc/team3256/robot/subsystems/Hanger.java
@@ -2,12 +2,36 @@ package frc.team3256.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import frc.team3256.robot.constants.DriveTrainConstants;
+import frc.team3256.warriorlib.loop.Loop;
 import frc.team3256.warriorlib.subsystem.SubsystemBase;
 import static frc.team3256.robot.constants.DriveTrainConstants.*;
 
-public class Hanger extends SubsystemBase {
+public class Hanger extends SubsystemBase implements Loop {
     private static Hanger instance;
     private DoubleSolenoid hang;
+
+    private enum HangerState {
+        HANGING,
+        RETRACTED
+    }
+
+    private enum WantedState {
+        WANTS_TO_HANG,
+        WANTS_TO_RETRACT
+    }
+
+
+
+    private HangerState mCurrentState = HangerState.RETRACTED;
+    private WantedState mWantedState = WantedState.WANTS_TO_RETRACT;
+
+    public void getHangerState() {
+        return m
+    }
+    public void setWantedState(WantedState wantedState){
+        this.mWantedState = wantedState;
+    }
+
 
     private Hanger() {
         hang = new DoubleSolenoid(DriveTrainConstants.pcmId, kHangerForward, kHangerReverse);
