363a8298fc42346a97b47a08097104c4997da744
Nic Pham <nicholas.pham@warriorlife.net>
2019-11-11 15:31:47 -0800

    updating ramsete controller

--- src/main/java/frc/team3256/robot/constants/Constants.java
+++ src/main/java/frc/team3256/robot/constants/Constants.java
@@ -8,19 +8,6 @@ public class Constants {
     public static final int kRightDriveMaster = 5; //3
     public static final int kRightDriveSlave = 6; //4
 
-    public static final int kPivot = 7;
-
-    public static final int pcmOneId = 15;
-    public static final int pcmTwoId = 0;
-
-    public static final int rightEjectForward = 0;
-    public static final int rightEjectBack = 2;
-    public static final int leftEjectForward = 4;
-    public static final int leftEjectBack = 3;
-    public static final int leftPopForward = 3;
-    public static final int leftPopBack = 2;
-    public static final int rightPopForward = 7;
-    public static final int rightPopBack = 6;
 
     public static final double kGearRatio = 6.4;
     public static final int kVelocityHighGearSlot = 1;
